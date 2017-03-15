8c6afc788b704dffc1acc00c4753b7ee69df2585
elenatsoi <elenatsoi@gmail.com>
2017-03-15 01:05:53 -0700

    jdbcMain.java

--- src/cecs323_jdbcproject/jdbcMain.java
+++ src/cecs323_jdbcproject/jdbcMain.java
@@ -38,20 +38,34 @@ public class jdbcMain {
     
 
     try {
+        
         //STEP 2: Register JDBC driver
+        
         Class.forName("org.apache.derby.jdbc.ClientDriver");
 
+        
         //STEP 3: Open a connection
+        
         conn = DriverManager.getConnection(DB_URL);
 
         
         //STEP 4: Execute a query
+        
         stmt = conn.createStatement();
         ResultSet rs = stmt.executeQuery(sql);
 
-        
+                
         //STEP 5: Extract data from result set
-
+        
+        System.out.printf(displayFormat, "ID", "First Name", "Last Name", "Phone #");
+        
+        while (rs.next()) {
+            //Retrieve by column name
+            
+            
+            //Display values
+            System.out.printf(displayFormat, VARIABLES);
+        }
         
 
         //STEP 6: Clean-up environment
